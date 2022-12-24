76bff5bfb8f90f8a7728f6b1e1661fc8901760b2
MariaSoroks <sormash@gmail.com>
2022-12-24 22:21:38 +0600

    Fixinggg

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -4,7 +4,7 @@ public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
         if (month==1 || month==3) System.out.println("31");
         if (month==2) System.out.println("28");
-        if (month==2 || month==6) System.out.println("30");
+        if (month==4 || month==6) System.out.println("30");
         if (month>12 || month<1) System.out.println("wrong number!");
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -6,7 +6,7 @@ public class Seasons {
         if (month >= 3 && month <=5) System.out.println("Spring");
         if(month>=6 && month <=8) System.out.println("Summer");
         if (month >=9 && month <=11) System.out.println("Autumn");
-        else System.out.println ("wrong number!");
+        if (month<1 || month>12) System.out.println ("wrong number!");
 
     }
 }
