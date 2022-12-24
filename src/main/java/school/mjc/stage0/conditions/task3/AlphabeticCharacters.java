11daacf2732d588e660e33b60b381c7fa1925f20
MariaSoroks <sormash@gmail.com>
2022-12-24 21:54:42 +0600

    Fixing

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if (character=='O' || character=='o') System.out.println("Vowel");
+        if (character=='B' || character=='b') System.out.println("Consonant");
+        else System.out.println("wrong alphabet!");
     }
 }
